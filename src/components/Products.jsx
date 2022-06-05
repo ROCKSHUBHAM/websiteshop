d0581398b9bfd3538a7c399188bb5ad9ca9aad63
ROCKSHUBHAM <shubhampp91368136@gmail.com>
2022-06-05 17:49:50 +0530

    project

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,45 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import img1 from '../images/portfolio-1.jpg'
+import img2 from '../images/portfolio-2.jpg'
+
+import img3 from '../images/portfolio-3.jpg'
+
+import img4 from '../images/portfolio-4.jpg'
+
+import img5 from '../images/portfolio-5.jpg'
+
+import img6 from '../images/portfolio-6.jpg'
 
 const Products = () => {
     return (
         <div>
+            <div id="portfolio" className="portfolio min-vh-100 pt-5">
 
-            <div id="portfolio" class="portfolio min-vh-100 pt-5">
-
-                <div class="container mt-5" data-aos="fade-up">
+                <div className="container mt-5" data-aos="fade-up">
 
                     <header className="section-header my-4">
                         <h1 className='border-bottom border-5 border-danger d-inline'>Portfolio</h1>
-                        {/* <p>Check our latest work</p> */}
                     </header>
-                    <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
-
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-app">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                    <div className="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-app">
+                            <div className="portfolio-wrap">
+                                <img src={img1} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
                                 </div>
                             </div>
                         </Link>
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-web">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
-                                    <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
-                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
-
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-app">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-web">
+                            <div className="portfolio-wrap">
+                                <img src={img2} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
@@ -47,10 +43,10 @@ const Products = () => {
                             </div>
                         </Link>
 
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-card">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-app">
+                            <div className="portfolio-wrap">
+                                <img src={img3} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
@@ -58,10 +54,10 @@ const Products = () => {
                             </div>
                         </Link>
 
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-web">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-card">
+                            <div className="portfolio-wrap">
+                                <img src={img4} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
@@ -69,10 +65,10 @@ const Products = () => {
                             </div>
                         </Link>
 
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-app">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-web">
+                            <div className="portfolio-wrap">
+                                <img src={img5} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
@@ -80,10 +76,10 @@ const Products = () => {
                             </div>
                         </Link>
 
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-card">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-app">
+                            <div className="portfolio-wrap">
+                                <img src={img6} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
@@ -91,10 +87,10 @@ const Products = () => {
                             </div>
                         </Link>
 
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-card">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-card">
+                            <div className="portfolio-wrap">
+                                <img src={img1} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
@@ -102,10 +98,21 @@ const Products = () => {
                             </div>
                         </Link>
 
-                        <Link to="/productview" class="col-lg-4 col-md-6 portfolio-item filter-web">
-                            <div class="portfolio-wrap">
-                                <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="" />
-                                <div class="portfolio-info">
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-card">
+                            <div className="portfolio-wrap">
+                                <img src={img2} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
+                                    <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
+
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link to="/productview" className="col-lg-4 col-md-6 portfolio-item filter-web">
+                            <div className="portfolio-wrap">
+                                <img src={img3} className="img-fluid" alt="..." />
+                                <div className="portfolio-info">
                                     <h4 className='fw-bolder'>Lorem ipsum dolor</h4>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic praesentium eveniet similique non nemo exercitationem alias dolorum impedit dolor voluptas veritatis animi, suscipit iure magni deleniti nobis! Ducimus, quod ab.</p>
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
 
 
